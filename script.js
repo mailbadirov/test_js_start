e0da64438f95055ea9862933be1b0892c8344bdf
mailbadirov <103843866+mailbadirov@users.noreply.github.com>
2022-10-28 14:11:19 +0300

    release 0.1

---
+++ script.js
@@ -0,0 +1,21 @@
+const mainTextArea = document.querySelector('.word-counter'),
+  lineCount = document.querySelector('.lines'),
+  wordCount = document.querySelector('.words'),
+  letterCount = document.querySelector('.letters'),
+  charCount = document.querySelector('.chars');
+
+mainTextArea.addEventListener('input', (event) => {
+  const textAreaValue = event.target.value;
+
+  lineCount.textContent = 0;
+  wordCount.textContent = 0;
+  letterCount.textContent = 0;
+  charCount.textContent = 0;
+
+  if (textAreaValue.length > 0) {
+    lineCount.textContent = textAreaValue.split('\n').length;
+    wordCount.textContent = textAreaValue.trim().split(' ').length;
+    letterCount.textContent = textAreaValue.match(/[a-z]/gi).length;
+    charCount.textContent = textAreaValue.match(/\S/g).length;
+  }
+});
